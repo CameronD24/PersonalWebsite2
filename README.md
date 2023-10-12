9b3b800b4caaf1706bb2c404fbdbe7a1b21dde98
Cameron Dixon <cam@camerons-mbp.mynetworksettings.com>
2023-10-12 12:27:36 -0400

    part2

---
+++ README.md
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+# PersonalSite
+
+<!-- This is my first personal website I completed this project by studying W3schools as well as webdocs. During my time completing this project i also used Gustavo's site as inspiration. I used flex a lot during this project as well as some other css tools. I also attempted using Js for changing the cursor... that didn't work. -->
+=======
+# PersonalWebsite2
+Part 2
+>>>>>>> b33012a34a52391088599bd0c81a5e88ab8822fd
---
+++ javascript.js
@@ -0,0 +1,12 @@
+// const cursorDot = document.querySelector("[data-cursor-dot]");
+// const cursorOutline = document.querySelector("[data-cursor-outline]");
+// window.addEventListener("mousemove", function(e) {
+//   const posX = e.clientX;
+//   const posY = e.clientY;
+
+//   cursorDot.computedStyleMap.left = `${posX}px`;
+//   cursorDot.computedStyleMap.top = `${posY}px`;
+  
+//   cursorOutline.computedStyleMap.left = `${posX}px`;
+//   cursorOutline.computedStyleMap.top = `${posY}px`;
+// });
